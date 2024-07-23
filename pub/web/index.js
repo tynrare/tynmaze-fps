467815ed531c19aa1b4e8efc150dc9b1e4781b38
tynroar <tynrare@gmail.com>
2024-07-23 18:54:22 +0300

    amend web build

--- pub/web/index.js
+++ pub/web/index.js
@@ -16,7 +16,7 @@ var Module = typeof Module != 'undefined' ? Module : {};
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp0zchsunk.js
+// include: /tmp/tmpuoc1yo41.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -197,7 +197,7 @@ Module['FS_createPath']("/res", "pic", true, true);
 
   })();
 
-// end include: /tmp/tmp0zchsunk.js
+// end include: /tmp/tmpuoc1yo41.js
 
 
 // Sometimes an existing Module object exists with properties
